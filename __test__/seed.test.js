d015eda0416b27b321d29e4f86ef3faaa6d70818
lewis912 <lewiscc93@gmail.com>
2025-11-24 15:51:53 +0000

    add test for users table

---
+++ __test__/seed.test.js
@@ -0,0 +1,26 @@
+const db = require("../db/connection");
+const seed = require("../db/seeds/seed");
+const data = require("../db/data/test-data/index");
+
+beforeAll(() => seed(data));
+afterAll(() => db.end());
+
+describe("seeding", () => {
+  describe("users table", () => {
+    test("users table exists", () => {
+      return db
+        .query(
+          `SELECT EXISTS (
+            SELECT FROM 
+                information_schema.tables
+            WHERE 
+                table_name = 'users'
+            );`
+        )
+        .then(({ rows: [{ exists }] }) => {
+            // console.log(rows)
+          expect(exists).toBe(true);
+        });
+    });
+  });
+});
