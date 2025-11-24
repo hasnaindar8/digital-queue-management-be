8f80e714c7ab17b1dc80ab6d9d8c167b7c52b37a
lewis912 <lewiscc93@gmail.com>
2025-11-24 15:56:39 +0000

    add test and dev data for seeding

---
+++ db/data/development-data/reasons.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    description: "prescription",
+    estimated_wait: "2 mins 30 secs",
+  },
+  {
+    description: "same day care",
+    estimated_wait: "3 mins",
+  },
+  {
+    description: "test results",
+    estimated_wait: "4 mins",
+  },
+];
---
+++ db/data/test-data/reasons.js
@@ -0,0 +1,14 @@
+module.exports = [
+  {
+    description: "reason 1",
+    estimated_wait: "x mins",
+  },
+  {
+    description: "reason 2",
+    estimated_wait: "y mins",
+  },
+  {
+    description: "reason 3",
+    estimated_wait: "z mins",
+  },
+];
