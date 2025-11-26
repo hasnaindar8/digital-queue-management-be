8fd798e8a3fc2cd365e22899d1dd4b9039ec9b92
lewis912 <lewiscc93@gmail.com>
2025-11-26 14:57:51 +0000

    add error tests

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -20,4 +20,20 @@ describe("DELETE /api/queue_entries/:entry_id", () => {
         expect(body).toEqual({})
     })
   });
+  test("400: Responds with error message when delete request is made with invalid entry_id type", () => {
+    return request(app)
+    .delete("/api/queue_entries/not_a_real_id")
+    .expect(400)
+    .then(({body: {msg}}) => {
+        expect(msg).toBe("Invalid input syntax")
+    })
+  }); 
+  test("404: Responds with error message when delete request is made with valid entry_id that does not exist", () => {
+    return request(app)
+    .delete("/api/queue_entries/50000")
+    .expect(404)
+    .then(({body: {msg}}) => {
+        expect(msg).toBe("Not Found")
+    })
+  });
 });
